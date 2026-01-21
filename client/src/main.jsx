3ad1eaae00f70bcd8f2b59255c714b00f83737b2
Mansha1505 <bajpaimb15@gmail.com>
2026-01-21 20:26:30 +0530

    Fix JSX entry file for Vercel build

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // or style.css if that exists
+import "./index.css";
 
 ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root")
 ).render(
   <React.StrictMode>
     <App />
